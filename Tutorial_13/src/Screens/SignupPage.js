d1ad0f7288f7c18922e5b061a8ed6d46bfe1b2ff
Limtotage <abdullahltgyalim@gmail.com>
2025-04-17 08:50:50 +0300

    Update and Delete Interaction made pressable

---
+++ Tutorial_13/src/Screens/SignupPage.js
@@ -0,0 +1,105 @@
+import {
+  Text,
+  Image,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Pressable,
+} from 'react-native';
+import { CustomTextInput, CustomButton } from '../components/index';
+import React, { useState } from 'react';
+const SignupPage = ({ navigation }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.title}>
+        <Image
+          source={require('../../assets/Images/Sign-Up.png')}
+          style={styles.image}
+        />
+        <Text style={styles.welcome}> Sign up </Text>
+      </View>
+      <View style={styles.textInputField}>
+        <CustomTextInput
+          handletitle="Name"
+          handleisSecureText={false}
+          handleonChangeText={setName}
+          handlevalue={name}
+          handleplaceholder="Enter Your Name"
+        />
+        <CustomTextInput
+          handletitle="E-Mail"
+          handleisSecureText={false}
+          handleonChangeText={setEmail}
+          handlevalue={email}
+          handleplaceholder="Enter Your E-mail"
+        />
+        <CustomTextInput
+          handletitle="Password"
+          handleisSecureText={true}
+          handleonChangeText={setPassword}
+          handlevalue={password}
+          handleplaceholder="Create a Password"
+        />
+      </View>
+      <View style={styles.signupButtonandothers}>
+        <CustomButton
+          buttonText="Sign Up"
+          setWidth="70%"
+          handleOnPress={() => console.log(name, ' ', email, ' ', password)}
+          pressedColor="lightgray"
+          nonPressedColor="blue"
+        />
+        <Pressable
+          style={{ justifyContent: 'space-between' }}
+          onPress={() => navigation.navigate('Login')}>
+          <Text style={{ fontWeight: 'bold', justifyContent: 'space-between' }}>
+            Already have a account? Login{' '}
+          </Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default SignupPage;
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightblue',
+  },
+  title: {
+    flex: 2,
+    paddingTop: 50,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInputField: {
+    flex: 2,
+    alignItems: 'center',
+    paddingVertical: 10,
+    width: '100%',
+    justifyContent: 'space-evenly',
+  },
+  signupButtonandothers: {
+    flex: 2,
+    width: '100%',
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+
+  welcome: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginBottom: 30,
+  },
+});
